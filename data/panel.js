21a49f420f790dd65a7fa260a0a3600c75fd3b85
Erik Wegner <e_wegner@web.de>
2014-08-06 21:38:42 +0200

    New detail list

--- data/panel.js
+++ data/panel.js
@@ -5,15 +5,13 @@ var servicetemplate = _.template($('#servicetemplate').html());
 
 //  global variables
 var detailstable = null;
-var servicecssclasseven = false;
+var hostcontainer = null;
 
 function processServiceDetail(servicedetail) {
     // output the details for a service
     servicedetail.actions = "R A";
     servicedetail.acknowledged = servicedetail.has_been_acknowledged === true ? "A" : "";
-    servicedetail.trcssclass = servicecssclasseven ? "even" : "odd";
-    detailstable.append(servicetemplate(servicedetail));
-    servicecssclasseven = !servicecssclasseven;
+    hostcontainer.append(servicetemplate(servicedetail));
 }
 
 function processHostDetail(hostdetail) {
@@ -21,14 +19,16 @@ function processHostDetail(hostdetail) {
     hostdetail.actions = "R A";
     hostdetail.acknowledged = hostdetail.has_been_acknowledged === true ? "A" : "";
     detailstable.append(hosttemplate(hostdetail));
+    hostcontainer = detailstable.find('.services').last();
     _(hostdetail.services).each(processServiceDetail);
 }
 
 self.port.on("ProcessStatusUpdate", function(status) {
     // this function refreshes the tables
     $('body').html(maintemplate(status));
-    detailstable = $('table#details');
+    detailstable = $('#details');
     _(status.details).each(processHostDetail);
+    filterDetails(0);
 });
 
 self.port.on("GenericError", function(message) {
@@ -44,3 +44,30 @@ var triggerRefresh = function() {
 }
 
 $(document).on('click', '.refresh', null, triggerRefresh);
+
+function filterDetails(a) {
+    // 0 => only problems
+    if (a!=parseInt(a)) {
+        a = parseInt($(this).text());
+    }
+    
+    function showParentHost() {
+        $(this).parent().parent().find(".level1").show();
+    }
+    
+    $('.level4').hide();
+    $('.level2').hide();
+    $('.level1').hide();
+    detailstable.find(".service > .status").filter(function(e) {return !$(this).hasClass("OK")}).parent().show().each(showParentHost).find('.level4').show();
+    detailstable.find(".host > .status").filter(function(e) {return !e.hasClass("OK")}).show();
+    
+    if (a > 0) {
+        $('.level1').show();
+        if (a > 1) {
+            $('.level2').show();
+            if (a > 2) {$('.level4').show();}
+        }
+    }
+}
+
+$(document).on('click', '.btnFilterDetails', filterDetails);
