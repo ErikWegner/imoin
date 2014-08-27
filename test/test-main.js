9a4727839e396e1359b60ba2fb7a71a33daf5c94
Erik Wegner <e_wegner@web.de>
2014-08-27 18:03:15 +0200

    Access to Firefox password manager

--- test/test-main.js
+++ test/test-main.js
@@ -9,4 +9,14 @@ exports["test main async"] = function(assert, done) {
   done();
 };
 
+exports["test url to host"] = function(assert) {
+  var cuthost = main.cuthost;
+  
+  assert.equal("https://www.example.com", cuthost("https://www.example.com/icinga/cgi-bin"), "cuthost 1");
+  assert.equal("https://www.example.com", cuthost("https://www.example.com/icinga/cgi-bin/"), "cuthost 2");
+  assert.equal("https://www.example.com", cuthost("https://www.example.com/"), "cuthost 3");
+  assert.equal("https://www.example.com", cuthost("https://www.example.com"), "cuthost 4");
+  assert.equal("https://www.example.com:80", cuthost("https://www.example.com:80/"), "cuthost 5");
+}
+
 require("sdk/test").run(exports);
