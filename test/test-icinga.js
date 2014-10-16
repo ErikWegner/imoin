f753f91186eb01e00b271d12e7c7228bf6b2fdd2
Erik Wegner <e_wegner@web.de>
2014-10-16 17:25:36 +0200

    Speed refactoring

--- test/test-icinga.js
+++ test/test-icinga.js
@@ -172,7 +172,6 @@ exports["test ProcessResponse_1_6"] = function(assert, done) {
 
 // ca. 3160 Services und 470 Hosts
 exports["test huge response"] = function(assert, done) {
-    var starttime = (new Date()).getTime();
     var EventNames = {
         InstanceUrlChanged: "InstanceUrlChanged",
         QueryStatus: "QueryStatus",
@@ -197,11 +196,11 @@ exports["test huge response"] = function(assert, done) {
         assert.pass("StatusUpdate");
         assert.equal(typeof status, "object", "status is an object");
         assert.equal(typeof status.details, "object", "status.details is an object");
-        assert.equal(500, status.totalhosts, "test totalhosts matches 500");
-        assert.equal(500*15, status.totalservices, "test totalservices matches");
+        assert.equal(8000, status.totalhosts, "test totalhosts matches 8000");
+        assert.equal(8000*20, status.totalservices, "test totalservices matches");
         var endtime = (new Date()).getTime();
         console.log("Duration: " + (endtime - starttime));
-        assert.ok(endtime - starttime < 200, "Fast enough");
+        assert.ok(endtime - starttime < 150, "Fast enough");
         done();
     });
     
@@ -212,9 +211,9 @@ exports["test huge response"] = function(assert, done) {
             "service_status": []
         }
     };
-    var hostlimit = 500;
+    var hostlimit = 8000;
     while (hostlimit-- > 0) {
-        var servicelimit = 15;
+        var servicelimit = 20;
         var hostname = "host" + hostlimit;
         bigresponse.status.host_status.push({"host_name": hostname, "host_display_name": hostname, "status": "UP", "last_check": "2014-07-10 21:43:53", "duration": "47d 23h 32m 40s", "attempts": "1/10", "state_type": "HARD", "is_flapping": false, "in_scheduled_downtime": false, "active_checks_enabled": true, "passive_checks_enabled": true, "notifications_enabled": true, "has_been_acknowledged": false, "action_url": "/pnp/graph?host=$HOSTNAME$' class='tips' rel='/pnp/popup?host=$HOSTNAME$&srv=_HOST_", "notes_url": null, "status_information": "PING OK - Packet loss = 0%, RTA = 0.07 ms"});
         while (servicelimit-- > 0) {
@@ -222,7 +221,7 @@ exports["test huge response"] = function(assert, done) {
             bigresponse.status.service_status.push({ "host_name": hostname, "host_display_name": hostname, "service_description": servicename, "service_display_name": servicename, "status": "OK", "last_check": "2014-07-10 21:20:10", "duration": "4d  9h 24m 45s", "attempts": "1/3", "state_type": "HARD", "is_flapping": false, "in_scheduled_downtime": false, "active_checks_enabled": true, "passive_checks_enabled": true, "notifications_enabled": true, "has_been_acknowledged": false, "action_url": "/pnp/graph?host=$HOSTNAME$&srv=$SERVICEDESC$' class='tips' rel='/pnp/popup?host=$HOSTNAME$&srv=$SERVICEDESC$", "notes_url": null, "status_information": "DRUPAL OK, ADMIN:OK=No known issues at this time., CRON:OK"});
         }
     }
-    
+    var starttime = (new Date()).getTime();
     icinga.ProcessResponse(bigresponse);
 }
 
