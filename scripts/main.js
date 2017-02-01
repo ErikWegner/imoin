9c2830d2b2d9003a1372b44b2404ca0779726e9f
Erik Wegner <e_wegner@web.de>
2017-02-01 16:03:47 +0100

    Del

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "./firefox"], function (require, exports, firefox_1) {
-    "use strict";
-    var e = new firefox_1.Firefox();
-    e.loadSettings().then(function (settings) {
-        e.initTimer(settings.timerPeriod, function () {
-            console.log("Alarm23");
-        });
-    });
-});
--- scripts/requireInit.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="definitions/requirejs/index.d.ts" />
-requirejs.config({
-    skipDataMain: true
-});
-require(['main'], function (main) {
-    console.log("all js loaded");
-});
