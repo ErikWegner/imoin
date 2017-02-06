c5bac4d2620ea78a6315e1ea02a72f044086c004
Erik Wegner <E_Wegner@web.de>
2017-02-06 18:54:56 +0100

    npm, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,46 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var tsProject = ts.createProject('firefox/tsconfig.json');
+var tsProject;
+var clean = require('gulp-clean');
+var es = require('event-stream');
 
-gulp.task('firefox-scripts', function() {
-        var tsResult = tsProject.src() 
-            .pipe(tsProject());
- 
-    return tsResult.js.pipe(gulp.dest('firefox'));
+var targetpaths = {};
+
+gulp.task('firefox-scripts', function () {
+    var tsResult = tsProject.src()
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(gulp.dest(targetpaths.target));
 });
+
+gulp.task('firefox-setpaths', function() {
+  tsProject = ts.createProject('firefox/tsconfig.json');
+  targetpaths.target = 'release/firefox';
+  targetpaths.icons = targetpaths.target + '/icons'
+  targetpaths.html = targetpaths.target + '/html'
+});
+
+gulp.task('copy-icons', function() {
+    return gulp.src(['icons/**/*']).pipe(gulp.dest(targetpaths.icons));
+})
+
+gulp.task('firefox', [
+    'firefox-setpaths', 
+    'copy-icons', 
+    'firefox-scripts',
+    ], function () {
+        return es.concat(
+            gulp.src(['firefox/html/**/*']).pipe(gulp.dest(targetpaths.html)),
+            gulp.src(['firefox/manifest.json']).pipe(gulp.dest(targetpaths.target))
+        )
+        
+})
+
+gulp.task('clean-firefox', ['firefox-setpaths'], function() {
+    return gulp.src(targetpaths.target, {read: false})
+    .pipe(clean());
+})
+
+gulp.task('clean', ['clean-firefox'], function() {
+    
+})
