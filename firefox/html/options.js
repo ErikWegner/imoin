15bdf117127e43b25a3daabd6becc44988e4685e
Erik Wegner <E_Wegner@web.de>
2017-02-12 21:16:34 +0100

    Options

--- firefox/html/options.js
+++ firefox/html/options.js
@@ -1,21 +1,30 @@
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.local.set({
-    url: document.querySelector("#url").value
+    timerPeriod: document.querySelector("#timerPeriod").value,
+    icingaversion: document.querySelector("#icingaversion").value,
+    url: document.querySelector("#url").value,
+    username: document.querySelector("#username").value,
+    password: document.querySelector("#password").value,
   });
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
+    document.querySelector("#timerPeriod").value = result.timerPeriod || "5";
+    document.querySelector("#icingaversion").value = result.icingaversion || "cgi";
     document.querySelector("#url").value = result.url || "";
+    document.querySelector("#username").value = result.username || "";
+    document.querySelector("#password").value = result.password || "";
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  var getting = browser.storage.local.get("color");
+  var getting = browser.storage.local.get(
+    ["timerPeriod", "icingaversion", "url", "username", "password"]);
   getting.then(setCurrentChoice, onError);
 }
 
--- scripts/Settings.ts
+++ scripts/Settings.ts
@@ -4,7 +4,9 @@ export class Settings {
     constructor(
         public timerPeriod = 5,
         public icingaversion: string,
-        public url: string
+        public url: string,
+        public username: string,
+        public password: string
     ) {
     }
 }
