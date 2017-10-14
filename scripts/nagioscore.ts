8cf56d53b8cead8272742f60fed31af333664baa
Erik Wegner <E_Wegner@web.de>
2017-10-14 17:42:29 +0200

    Tests and processing

--- scripts/nagioscore.ts
+++ scripts/nagioscore.ts
@@ -4,13 +4,28 @@ import { UICommand } from './UICommand';
 
 interface IHostJsonData {
   data: {
-    hostlist: {[hostname: string]: {
-      name: String
-    }};
+    hostlist: {
+      [hostname: string]: {
+        name: string,
+        status: number,
+        plugin_output: string
+      }
+    };
   }
 }
 
 interface IServiceJsonData {
+  data: {
+    servicelist: {
+      [servicename: string]: {
+        host_name: string,
+        description: string,
+        status: number,
+        last_check: number,
+        plugin_output: string
+      }
+    }
+  }
 
 }
 
@@ -25,16 +40,16 @@ export class NagiosCore extends AbstractMonitor {
         const servicesrequest = this.environment.load(servicesurl, this.settings.username, this.settings.password);
 
         Promise
-        .all([hostsrequest, servicesrequest])
-        .then(a => {
+          .all([hostsrequest, servicesrequest])
+          .then(a => {
             const hostdata = JSON.parse(a[0]);
             const servicedata = JSON.parse(a[1]);
             const m = this.processData(hostdata, servicedata);
             resolve(m);
-        })
-        .catch(a => {
+          })
+          .catch(a => {
             resolve(Monitor.ErrorMonitorData("Connection error. Check settings and log. " + a[0] + "|" + a[1]));
-        });
+          });
       });
   }
   handleUICommand(param: UICommand): void {
@@ -45,5 +60,37 @@ export class NagiosCore extends AbstractMonitor {
     if (hostdata == null || servicedata == null) {
       return Monitor.ErrorMonitorData("Result empty");
     }
+    const m = new Monitor.MonitorData();
+    let hostByName: { [name: string]: Monitor.Host } = {};
+    Object.keys(hostdata.data.hostlist).forEach((hostname) => {
+      const hostdatahost = hostdata.data.hostlist[hostname];
+      const host = new Monitor.Host(hostdatahost.name);
+      hostByName[host.name] = host;
+      host.setState(hostdatahost.status == 2 ? "UP" : "DOWN");
+      host.checkresult = hostdatahost.plugin_output;
+      m.addHost(host);
+    });
+
+    Object.keys(servicedata.data.servicelist).forEach((servicename) => {
+      const servicedataservice = servicedata.data.servicelist[servicename];
+      const host = hostByName[servicedataservice.host_name];
+      if (host) {
+        const service = new Monitor.Service(servicedataservice.description);
+        if (servicedataservice.last_check !== 0) {
+          if (servicedataservice.status === 2) {
+            service.setStatus("OK");
+          } else if (servicedataservice.status === 4) {
+            service.setStatus("WARNING");
+          }
+          service.checkresult = servicedataservice.plugin_output;
+        } else {
+          service.checkresult = "Check did not run yet.";
+        }
+        service.host = host.name;
+        host.addService(service);
+      }
+    });
+
+    return m;
   }
 }
