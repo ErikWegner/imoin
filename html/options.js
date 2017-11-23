9828679f341bda51dcc8f8f8f31eb5de81afa075
Erik Wegner <E_Wegner@web.de>
2017-11-23 21:03:44 +0100

    Event handler stubs

--- html/options.js
+++ html/options.js
@@ -1,28 +1,28 @@
-var hosttype = chrome ? "chrome" : browser ? "browser" : "na"
+var hosttype = chrome ? 'chrome' : browser ? 'browser' : 'na'
 var host = chrome || browser;
 
-function saveOptions(e) {
-  e.preventDefault();
+function saveOptions() {
   host.storage.local.set({
-    timerPeriod: parseInt(document.querySelector("#timerPeriod").value),
-    icingaversion: document.querySelector("#icingaversion").value,
-    url: document.querySelector("#url").value,
-    username: document.querySelector("#username").value,
-    password: document.querySelector("#password").value,
+    timerPeriod: parseInt(document.querySelector('#timerPeriod').value),
+    icingaversion: document.querySelector('#icingaversion').value,
+    url: document.querySelector('#url').value,
+    username: document.querySelector('#username').value,
+    password: document.querySelector('#password').value,
   });
-  var myPort = host.runtime.connect({name:"port-from-options"});
-  myPort.postMessage({command: "SettingsChanged"});
+  var myPort = host.runtime.connect({name:'port-from-options'});
+  myPort.postMessage({command: 'SettingsChanged'});
   myPort.disconnect();
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    document.querySelector("#timerPeriod").value = result.timerPeriod || "5";
-    document.querySelector("#icingaversion").value = result.icingaversion || "cgi";
-    document.querySelector("#url").value = result.url || "";
-    document.querySelector("#username").value = result.username || "";
-    document.querySelector("#password").value = result.password || "";
+    document.querySelector('#instancelabel').value = result.label || 'Default';
+    document.querySelector('#timerPeriod').value = result.timerPeriod || '5';
+    document.querySelector('#icingaversion').value = result.icingaversion || 'cgi';
+    document.querySelector('#url').value = result.url || '';
+    document.querySelector('#username').value = result.username || '';
+    document.querySelector('#password').value = result.password || '';
   }
 
   function onError(error) {
@@ -34,19 +34,29 @@ function restoreOptions() {
   }
 
   /* Change the array of keys to match the firefox.ts */
-  if (hosttype == "browser") {
+  if (hosttype == 'browser') {
     var getting = host.storage.local.get(
-      ["timerPeriod", "icingaversion", "url", "username", "password"]);
+      ['timerPeriod', 'icingaversion', 'url', 'username', 'password']);
     getting.then(setCurrentChoice, onError);
-  } else if (hosttype == "chrome") {
-    host.storage.local.get(["timerPeriod", "icingaversion", "url", "username", "password"], setCurrentChoice);
+  } else if (hosttype == 'chrome') {
+    host.storage.local.get(['timerPeriod', 'icingaversion', 'url', 'username', 'password'], setCurrentChoice);
   }
 }
 
 function addInstance() {
   const i = document.getElementById('instanceid');
-  i.options[i.options.length] = new Option("Instance " + i.options.length, i.options.length);
+  i.options[i.options.length] = new Option('Instance ' + i.options.length, i.options.length);
 }
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+function updateInstance() {
+
+}
+
+function removeInstance() {
+
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('#addInstance').addEventListener('click', addInstance);
+document.querySelector('#updateInstance').addEventListener('click', updateInstance);
+document.querySelector('#removeInstance').addEventListener('click', removeInstance);
