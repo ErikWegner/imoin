6ca54a05ec5b904008cf10175e6551538bcd10df
Erik Wegner <E_Wegner@web.de>
2017-12-05 19:26:41 +0100

    MockAbstractEnvironment

---
+++ test/abstractHelpers/MockAbstractEnvironment.ts
@@ -0,0 +1,38 @@
+import { AbstractEnvironment } from '../../scripts/AbstractEnvironment';
+import { Settings } from '../../scripts/Settings';
+
+export class MockAbstractEnvironment extends AbstractEnvironment {
+  load(url: string, username: string, password: string): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+  post(url: string, data: any, username: string, password: string): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+  debug(o: any): void {
+    throw new Error("Method not implemented.");
+  }
+  log(o: any): void {
+    throw new Error("Method not implemented.");
+  }
+  error(o: any): void {
+    throw new Error("Method not implemented.");
+  }
+  loadSettings(): Promise<Settings> {
+    throw new Error("Method not implemented.");
+  }
+  initTimer(index: number, delay: number, callback: () => void): void {
+    throw new Error("Method not implemented.");
+  }
+  stopTimer(index: number): void {
+    throw new Error("Method not implemented.");
+  }
+  protected trySendDataToPopup(): void {
+    throw new Error("Method not implemented.");
+  }
+  protected openWebPage(url: string): void {
+    throw new Error("Method not implemented.");
+  }
+  protected updateIconAndBadgetext(): void {
+    throw new Error("Method not implemented.");
+  }
+} 
---
+++ test/testAbstractEnvironment.ts
@@ -0,0 +1,7 @@
+import 'mocha';
+import { assert, expect } from 'chai';
+import { MockAbstractEnvironment } from './abstractHelpers/MockAbstractEnvironment';
+
+describe('AbstractEnvironnment', () => {
+  
+});
